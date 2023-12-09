5ac5a855a23e1434760d87e15c19032480f3669e
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2023-12-09 20:05:36 +0300

    some data types overviews

---
+++ some_data_types_overviews.py
@@ -0,0 +1,20 @@
+# ------------------------------------------------
+# type() => get the data type
+# All Data in Python is Object
+# ------------------------------------------------
+
+print(type(10)) # int => Integer
+print(type(100)) # int => Integer
+print(type(-50)) # int => Integer
+
+print(type(100.9)) # float => Floating Point Number
+print(type(1.485869)) # float => Floating Point Number
+print(type(-80.5)) # float => Floating Point Number
+
+print(type("Hi Marwa")) # str => String
+
+print(type( [1, 2, 3, 4, 5] )) # list => List
+print(type( (1, 2, 3, 4, 5) )) # tuple => Tuple
+print(type( { "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, } )) # Dict => dictionary
+
+print(type(2 == 2)) # bool => Boolean
