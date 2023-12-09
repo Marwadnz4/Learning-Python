19dc1e546aee0f95ba475ae52f87ba89eb956daf
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2023-12-09 19:45:54 +0300

    Create comments file

---
+++ comments.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------
+# What I can write in comments :
+# Information about The File
+# License
+# Who created The File
+# When The File created
+# Why The File created
+#
+# when I can write comments :
+# w
+# ------------------------------------------------
+
+# This is comment
+print("Programming") # This is inline comment
+
+"""
+This 
+is 
+not
+multi line
+comment.
+This is a String
+
+"""
