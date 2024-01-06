7afb6e43efe59731c0d4c1d1c3068c525f223321
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2024-01-06 19:03:03 +0300

    variables part one

---
+++ variables_part_one.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------
+# -- Variables --
+# ---------------
+#
+# Name Convention and Rules
+# [1] Can start with (a-z A-Z) or underscore like myVar, _myVar
+# [2] You Cannot Start with Num or Special Characters like @yVar, 100var, -var
+# [3] Can include (0-9) or Underscore like my100Var, my_var
+# [4] Cannot include Special Characters my@var
+# [5] Name is Not Like name [ case Sensitive ]
+# -----------------------------------------------------
+
+name = "Marwa Mohammed" # single Word => Normal
+print(name)
+Name = "Marwa Mohammed" # Two Words => camelCase
+print(Name)
+my_name = "Marwa Mohammed" # Two Words => snake_case
+print(my_name)
